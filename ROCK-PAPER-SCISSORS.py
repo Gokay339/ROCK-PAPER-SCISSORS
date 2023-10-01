41851666045887427e1a10ce0e5d7c4716d6ac51
Gökay Adıgüzel <116993938+Gokay339@users.noreply.github.com>
2023-10-02 01:48:25 +0300

    Update ROCK-PAPER-SCISSORS.py

--- ROCK-PAPER-SCISSORS.py
+++ ROCK-PAPER-SCISSORS.py
@@ -1,22 +1,23 @@
 from random import choice
 
-# Printing a header for the game
 print("#" * 80)
-print("#" * 27 + "  Rock - Paper - Scissors  " + "#" * 26)
-print("#" * 27 + "    Welcome to the Game    " + "#" * 26)
+print("#" * 30 + " Rock, Paper, Scissors " + "#" * 29)
+print("#" * 29 + " Welcome to the Game " + "#" * 29)
 print("#" * 80)
+print("Press 'q' to Exit the Game.")
 
-print("To Quit the Game, Press q Key.")
 while True:
-    print("Choose an Option to Play\n"
-          "1. Single Round\n"
-          "2. Best of Three\n")
+    print("""Choose the Option You Want to Play:\n
+    1. Single Round
+    2. First to 3 Wins\n""")
+
     try:
-        game_choice = input("Choice: ")
+        game_choice = input("Option: ")
+
         if game_choice == "1" or game_choice == "2":
             game_choice = int(game_choice)
         elif game_choice == "q" or game_choice == "Q":
-            print("Game Over ...")
+            print("Game Over...")
             exit()
         else:
             print("You made an invalid choice. Please enter '1' or '2' or 'q'.\n")
@@ -29,7 +30,7 @@ while True:
         if game_choice == 1:
             computer_choice = choice(["rock", "paper", "scissors"])
             computer_choice = computer_choice.upper()
-            print("Rock, Paper, or Scissors - Type Your Choice")
+            print("Rock, Paper, Scissors - Enter Your Choice ")
 
             player_choice = str(input("Your Turn: "))
             player_choice = player_choice.upper()
@@ -40,128 +41,126 @@ while True:
             if player_choice == "ROCK":
                 if computer_choice == "ROCK":
                     print("Computer's Choice: Rock")
-                    print("Result: Tie!\n")
-                    print("Do You Want to Play Again? Press 'q' to Quit ...\n")
+                    print("Result: Draw!\n")
+                    print("""Do You Want to Play Again? 
+    Press 'q' to Exit...\n""")
 
                 elif computer_choice == "PAPER":
                     print("Computer's Choice: Paper")
-                    print("Result: You Lose!\n")
-                    print("Do You Want to Play Again? Press 'q' to Quit ...\n")
+                    print("Result: You Lost!\n")
+                    print("""Do You Want to Play Again? 
+    Press 'q' to Exit...\n""")
 
                 else:
                     print("Computer's Choice: Scissors")
-                    print("Result: You Win!\n")
-                    print("Do You Want to Play Again? Press 'q' to Quit ...\n")
+                    print("Result: You Won!\n")
+                    print("""Do You Want to Play Again? 
+    Press 'q' to Exit...\n""")
 
             elif player_choice == "PAPER":
                 if computer_choice == "PAPER":
                     print("Computer's Choice: Paper")
-                    print("Result: Tie!\n")
-                    print("Do You Want to Play Again? Press 'q' to Quit ...\n")
+                    print("Result: Draw!\n")
+                    print("""Do You Want to Play Again? 
+    Press 'q' to Exit...\n""")
 
                 elif computer_choice == "ROCK":
                     print("Computer's Choice: Rock")
-                    print("Result: You Win!\n")
-                    print("Do You Want to Play Again? Press 'q' to Quit ...\n")
+                    print("Result: You Won!\n")
+                    print("""Do You Want to Play Again? 
+    Press 'q' to Exit...\n""")
 
                 else:
                     print("Computer's Choice: Scissors")
-                    print("Result: You Lose!\n")
-                    print("Do You Want to Play Again? Press 'q' to Quit ...\n")
+                    print("Result: You Lost!\n")
+                    print("""Do You Want to Play Again? 
+    Press 'q' to Exit...\n""")
 
             elif player_choice == "SCISSORS":
                 if computer_choice == "SCISSORS":
                     print("Computer's Choice: Scissors")
-                    print("Result: Tie!\n")
-                    print("Do You Want to Play Again? Press 'q' to Quit ...\n")
+                    print("Result: Draw!\n")
+                    print("""Do You Want to Play Again? 
+    Press 'q' to Exit...\n""")
 
                 elif computer_choice == "PAPER":
                     print("Computer's Choice: Paper")
-                    print("Result: You Win!\n")
-                    print("Do You Want to Play Again? Press 'q' to Quit ...\n")
+                    print("Result: You Won!\n")
+                    print("""Do You Want to Play Again? 
+    Press 'q' to Exit...\n""")
 
                 else:
                     print("Computer's Choice: Rock")
-                    print("Result: You Lose!\n")
-                    print("Do You Want to Play Again? Press 'q' to Quit ...\n")
+                    print("Result: You Lost!\n")
+                    print("""Do You Want to Play Again? 
+    Press 'q' to Exit...\n""")
 
             elif player_choice == "Q":
-                print("Game Over ...")
+                print("Game Over...")
                 exit()
 
         if game_choice == 2:
             game_score = {"player": 0, "computer": 0}
-            computer_choice = choice(["rock", "paper", "scissors"])
-            computer_choice = computer_choice.upper()
-            print("Rock, Paper, or Scissors - Type Your Choice")
 
-            player_choice = str(input("Your Turn: "))
-            player_choice = player_choice.upper()
-            if player_choice != "ROCK" and player_choice != "PAPER" and player_choice != "SCISSORS" and player_choice != "Q":
-                print("You made an invalid choice.\nPlease enter 'rock', 'paper', 'scissors', or 'q'.\n")
-                continue
+            while True:
+                computer_choice = choice(["rock", "paper", "scissors"])
+                computer_choice = computer_choice.upper()
+                print("Rock, Paper, Scissors - Enter Your Choice ")
+                player_choice = str(input("Your Turn: "))
+                player_choice = player_choice.upper()
 
-            if player_choice == "ROCK":
-                if computer_choice == "ROCK":
-                    print("Computer's Choice: Rock")
-                    print("Result: Tie!\n")
-                    print(f" Scores  You : {game_score['player']}  and  Computer : {game_score['computer']}")
+                if player_choice != "ROCK" and player_choice != "PAPER" and player_choice != "SCISSORS" and player_choice != "Q":
+                    print("You made an invalid choice.\nPlease enter 'rock', 'paper', 'scissors', or 'q'.\n")
+                    continue
 
-                elif computer_choice == "PAPER":
-                    print("Computer's Choice: Paper")
-                    print("Result: You Lose!\n")
-                    game_score['computer'] += 1
-                    print(f" Scores  You : {game_score['player']}  and  Computer : {game_score['computer']}")
+                if player_choice == "ROCK":
+                    if computer_choice == "ROCK":
+                        print("Computer's Choice: Rock")
+                        print("Result: Draw!\n")
+                    elif computer_choice == "PAPER":
+                        print("Computer's Choice: Paper")
+                        print("Result: You Lost!\n")
+                        game_score['computer'] += 1
+                    else:
+                        print("Computer's Choice: Scissors")
+                        print("Result: You Won!\n")
+                        game_score['player'] += 1
 
-                else:
-                    print("Computer's Choice: Scissors")
-                    print("Result: You Win!\n")
-                    game_score['player'] += 1
-                    print(f" Scores  You : {game_score['player']}  and  Computer : {game_score['computer']}")
+                elif player_choice == "PAPER":
+                    if computer_choice == "PAPER":
+                        print("Computer's Choice: Paper")
+                        print("Result: Draw!\n")
+                    elif computer_choice == "ROCK":
+                        print("Computer's Choice: Rock")
+                        print("Result: You Won!\n")
+                        game_score['player'] += 1
+                    else:
+                        print("Computer's Choice: Scissors")
+                        print("Result: You Lost!\n")
+                        game_score['computer'] += 1
 
-            elif player_choice == "PAPER":
-                if computer_choice == "PAPER":
-                    print("Computer's Choice: Paper")
-                    print("Result: Tie!\n")
-                    print(f" Scores  You : {game_score['player']}  and  Computer : {game_score['computer']}")
+                elif player_choice == "SCISSORS":
+                    if computer_choice == "SCISSORS":
+                        print("Computer's Choice: Scissors")
+                        print("Result: Draw!\n")
+                    elif computer_choice == "PAPER":
+                        print("Computer's Choice: Paper")
+                        print("Result: You Won!\n")
+                        game_score['player'] += 1
+                    else:
+                        print("Computer's Choice: Rock")
+                        print("Result: You Lost!\n")
+                        game_score['computer'] += 1
 
-                elif computer_choice == "ROCK":
-                    print("Computer's Choice: Rock")
-                    print("Result: You Win!\n")
-                    game_score['player'] += 1
-                    print(f" Scores  You : {game_score['player']}  and  Computer : {game_score['computer']}")
+                print(f"Scores  You: {game_score['player']}  and  Computer: {game_score['computer']}")
 
-                else:
-                    print("Computer's Choice: Scissors")
-                    print("Result: You Lose!\n")
-                    game_score['computer'] += 1
-                    print(f" Scores  You : {game_score['player']}  and  Computer : {game_score['computer']}")
+                if game_score['player'] == 3:
+                    print("Game over!\nYou Win!")
+                    exit()
+                if game_score['computer'] == 3:
+                    print("Game over!\nYou Lost!")
+                    exit()
 
-            elif player_choice == "SCISSORS":
-                if computer_choice == "SCISSORS":
-                    print("Computer's Choice: Scissors")
-                    print("Result: Tie!\n")
-                    print(f" Scores  You : {game_score['player']}  and  Computer : {game_score['computer']}")
-
-                elif computer_choice == "PAPER":
-                    print("Computer's Choice: Paper")
-                    print("Result: You Win!\n")
-                    game_score['player'] += 1
-                    print(f" Scores  You : {game_score['player']}  and  Computer : {game_score['computer']}")
-
-                else:
-                    print("Computer's Choice: Rock")
-                    print("Result: You Lose!\n")
-                    game_score['computer'] += 1
-                    print(f" Scores  You : {game_score['player']}  and  Computer : {game_score['computer']}")
-
-            if game_score['player'] == 3:
-                print("Game Over!\nYou Win!")
-                exit()
-            if game_score['computer'] == 3:
-                print("Game Over!\nYou Lose!")
-                exit()
-
-            elif player_choice == "Q":
-                print("Game Over ...")
-                exit()
+                elif player_choice == "Q":
+                    print("Game Over...")
+                    exit()
